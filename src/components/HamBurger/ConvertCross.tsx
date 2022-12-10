fe307b6368a90541fe2f29f393bca97152754210
Arpan Das <dasarpan471@gmail.com>
2022-12-10 17:02:31 +0530

    module css issue resolved

--- src/components/HamBurger/ConvertCross.tsx
+++ src/components/HamBurger/ConvertCross.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react/cjs/react.development";
 import "./styles/convert-cross.css";
-import "./styles/common.css";
+// import styles from "./styles/common.module.css"; uncomment this line and convert the common.css to common.module.css
 import "./styles/animations.css";
-
+import "./styles/common.css";
 function ConvertCross(): JSX.Element {
   const [classForActive, setClassForActive] = useState(false);
 
